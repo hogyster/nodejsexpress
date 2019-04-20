9f36ba05a0b13e21b927533bf6423a15b26100aa
hogyster <hogy@hogysterui-MacBook-Pro.local>
2019-04-20 18:59:30 +0900

    first Commit

---
+++ app.js
@@ -0,0 +1,40 @@
+//load our app server using express somehow...
+//import
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+
+app.get("/user/:id", (req, res) => {
+    console.log("Fetching user with id: " + req.params.id)
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root', 
+        password: 'root',
+        database: 'mydb'
+    })
+    connection.query("SELECT * FROM users", (err, rows, fields)=> {
+        console.log("I think we fetched users successfully")
+        res.json(rows)
+    })
+    // res.end()
+})
+
+app.get("/", (req, res) => {
+    console.log("Responding to root route")
+    res.send("Hello from Roooot")
+})
+
+app.get("/users", (req, res) => {
+    
+    var user1 = {firstName: "Stephan", lastName: "Curry"}
+    const user2 = {firstName: "Kevin", lastName: "Durant"}
+    res.json ([user1, user2])
+    // res.send("Nodemon auto updates when I save this file")
+})
+
+//localhost:3003
+app.listen(3006, () =>{
+    console.log("Server is up and listening on 3006...");
+    
+})
